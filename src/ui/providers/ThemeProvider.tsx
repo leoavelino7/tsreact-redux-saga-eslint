7f39de8a1843777395a357d259460cbed7db4739
Leonardo Avelino <leonardo.avelino16@hotmail.com>
2020-09-22 09:53:41 -0300

    Adicionando hook para lidar com localStorage

--- src/ui/providers/ThemeProvider.tsx
+++ src/ui/providers/ThemeProvider.tsx
@@ -1,34 +1,7 @@
-import React, {createContext, useState} from "react";
+import React, {createContext} from "react";
+import { useLocalStorage } from "../../data/hooks/useLocalStorage";
 
 import { GlobalStyles } from "../styles/global";
-
-function useLocalStorage(
-  key: string,
-  initialValue: string,
-): [string, Function] {
-  const [storedValue, setStoredValue] = useState(() => {
-    try {
-      const item = window.localStorage.getItem(key);
-      return item ? JSON.parse(item) : initialValue;
-    } catch (error) {
-      console.log(error);
-      return initialValue;
-    }
-  });
-
-  const setValue = (valueToStore: string): void => {
-    try {
-      console.log('ist', valueToStore);
-      
-      setStoredValue(valueToStore);
-      window.localStorage.setItem(key, JSON.stringify(valueToStore));
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  return [storedValue, setValue];
-}
 
 interface ContextProps {
   theme: string;
@@ -36,7 +9,7 @@ interface ContextProps {
 }
 
 export const ThemeContext = createContext<ContextProps>({
-  theme: "",
+  theme: '',
   setTheme: () => null,
 });
 
@@ -44,7 +17,7 @@ interface Props {
   children: React.ReactNode;
 }
 
-export function ThemeProvider(props: Props): JSX.Element {
+export function ThemeProvider(props: Props){
   const [theme, setTheme] = useLocalStorage("theme", "light");
 
   return (
